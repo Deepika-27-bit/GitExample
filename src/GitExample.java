cff95255751c6e3f62e020c8675e20193ca830dd
Deepika-27-bit <85643586+Deepika-27-bit@users.noreply.github.com>
2021-08-02 17:45:04 +0530

    Update GitExample.java
    changes done

--- src/GitExample.java
+++ src/GitExample.java
@@ -3,7 +3,7 @@ public class GitExample {
 	public static void main(String[] args) {
 		System.out.println("Good evening!!");
 		System.out.println("Good afternoon!!");
-		System.out.println("Hai!!");
+		System.out.println("Hai!! how are you");
 		
 		
 	}
